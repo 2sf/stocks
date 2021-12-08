c04a77aa90af538add7a2a72d35548953156ab7c
Tausif Karim <2sfkarim@gmail.com>
2021-12-09 00:54:55 +0600

    fixed model and sample data

--- lib/models/mock_data.dart
+++ lib/models/mock_data.dart
@@ -1,9 +1,27 @@
 import 'company.dart';
+//List<String> years= ['2015','2016','2017','2018','2019','2020','2021','TTM'];
+// List<Company> samples = [
+//   Company(
+//     companyName: 'GRAMEENPHONE LTD. (GP)',
+//     companyInitials: 'GP',
+//     companyDescription: 'Grameenphone Ltd. is a telecommunications company. The Company provides a range of products and services, such as prepaid, including Nishchinto, Bondhu and Shohoj; postpaid, including Xplore, Xplore Legend and Business Solution Ekota; roaming, including outbound roaming and inbound roaming; Internet of things mobile for development (IOT M4D) and device, including handset feature phone and smartphone, and Internet and connected device third generation (3G) modem, 3G router, wearables and accessories; Internet 3G; Internet second generation (2G); enterprise solution, including voice message broadcast and vehicle tracking system; digital services, including Comoyo and wowbox; value added services, including instant messaging and miss call alert, and adjacent businesses, such as financial services and infrastructure services. It provides financial services in electronic ticketing, bill collection, electronic lottery and partner bank services under the brand MobiCash. \n Number of employees : 2 086 people.',
+//     totalRevenue = [104754372000,114862160000,128435814000,132831967000,143656271000,139606161000,2021,];
+//     //TODO add and set values of all the lists here 
+//   )
+// ];
 
-List<Company> samples = [
-  Company(
-    companyName: 'Grameen Phone',
-    
-  )
-];
-
+final company = Company(
+  companyName: 'GRAMEENPHONE LTD. (GP)',
+  companyInitials: 'GP',
+  companyDescription: 'Grameenphone Ltd. is a telecommunications company. The Company provides a range of products and services, such as prepaid, including Nishchinto, Bondhu and Shohoj; postpaid, including Xplore, Xplore Legend and Business Solution Ekota; roaming, including outbound roaming and inbound roaming; Internet of things mobile for development (IOT M4D) and device, including handset feature phone and smartphone, and Internet and connected device third generation (3G) modem, 3G router, wearables and accessories; Internet 3G; Internet second generation (2G); enterprise solution, including voice message broadcast and vehicle tracking system; digital services, including Comoyo and wowbox; value added services, including instant messaging and miss call alert, and adjacent businesses, such as financial services and infrastructure services. It provides financial services in electronic ticketing, bill collection, electronic lottery and partner bank services under the brand MobiCash. \n Number of employees : 2 086 people.',
+  totalRevenue : [104754372000,114862160000,128435814000,132831967000,143656271000,139606161000,2021,],
+  netIncome: [2015,2016,7422650000,33362570000,34516760000,37187040000,2021,],
+  netCashFlowFromOperations: [2015,2016,2017,2018,2019,2020,2021,],
+  netProfitAfterTax: [2015,2016,2017,2018,2019,2020,2021,],
+  totalOpratingIncome: [2015,2016,2017,2018,2019,2020,2021,],
+  interestExpense: [2015,2016,2017,2018,2019,2020,2021,],
+  currentAssets: [2015,2016,2017,2018,2019,2020,2021,],
+  currentLiabilities: [2015,2016,2017,2018,2019,2020,2021,],
+  totalDebt: [2015,2016,2017,2018,2019,2020,2021,],
+);
+ 	 		
